e558e7e89cb2e3994f1582f0dbc50df37d482dfa
Peter Applebee <peterapplebee@gmail.com>
2014-08-17 15:47:30 +0100

    Re-styled 2 and Added Footer

--- js/app.js
+++ js/app.js
@@ -1,27 +1,50 @@
 $(document).ready(function() {
-
+	//add items to list
 	$('form').on('click', '.add-button', function(event) {
 		event.preventDefault();
 		var newItem = $(this).closest('form').find('.item-field').val();
-		var newItemDiv = $('<div class="item"><button class="btn untick"></button><p>'+newItem+'</p><button class="btn delete"></button></div>');
+		var newItemDiv = $('<div class="item"><button class="btn untick"></button><p>'+newItem+'</p><button title="Delete" class="btn delete"></button></div>');
 		$('#list').prepend(newItemDiv);
+	})
+	//toggle blue border on item field when hovering button
+	.on('mouseenter', '.add-button', function() {
+		$(this).closest('form').find('.item-field').addClass('border-toggle-blue');
+	})
+	.on('mouseleave', '.add-button', function() {
+		$(this).closest('form').find('.item-field').removeClass('border-toggle-blue');
 	});
-
+	//toggle blue border on button when hovering item field
+	$('form').on('mouseenter', '.item-field', function() {
+		$(this).closest('form').find('.add-button').addClass('border-toggle-blue-cross-blue');
+	})
+	.on('mouseleave', '.item-field', function() {
+		$(this).closest('form').find('.add-button').removeClass('border-toggle-blue-cross-blue');
+	});
+	//toggle blue border on button when item fielf focused
+	$('.item-field').focusin(function () {
+		$(this).closest('form').find('.add-button').addClass('border-toggle-blue-cross-blue-2');
+	})
+	.focusout(function () {
+		$(this).closest('form').find('.add-button').removeClass('border-toggle-blue-cross-blue-2');
+	});
+	//tick item on list
 	$('#list').on('click', '.untick', function(event) {
 		event.preventDefault();
 		console.log('clicked');
 		$(this).closest('.item').addClass('done');
 		$(this).removeClass('untick');
 		$(this).addClass('tick');
+		$(this).prop('title', 'Untick');
 	});
-
+	//untick item on list
 	$('#list').on('click', '.tick', function(event) {
 		event.preventDefault();
 		$(this).closest('.item').removeClass('done');
 		$(this).removeClass('tick');
 		$(this).addClass('untick');
+		$(this).prop('title', 'Tick');
 	});
-
+	//permanently delete item from list
 	$('#list').on('click', '.delete', function(event) {
 		event.preventDefault();
 		$(this).closest('.item').remove();
