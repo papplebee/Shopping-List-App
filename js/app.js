a53068f1eaaaf10c67801d29754a042d92e942fd
Peter Applebee <peterapplebee@gmail.com>
2014-08-17 18:34:37 +0100

    Added jquery to notes textarea

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 	$('form').on('click', '.add-button', function(event) {
 		event.preventDefault();
 		var newItem = $(this).closest('form').find('.item-field').val();
-		var newItemDiv = $('<div class="item"><button class="btn untick"></button><p>'+newItem+'</p><button title="Delete" class="btn delete"></button></div>');
+		var newNotes = $(this).closest('#shopping-list-app').find('#notes-field').val();
+		var newItemDiv = $('<div class="item"><button class="btn untick"></button><p>'+newItem+'</p><div class="arrow-right"></div><div class="arrow-down"></div><button title="Delete" class="btn delete"></button><p class="extra-notes">'+newNotes+'</p></div>');
 		$('#list').prepend(newItemDiv);
-	})
+	});
+
 	//toggle blue border on item field when hovering button
-	.on('mouseenter', '.add-button', function() {
+	$('form').on('mouseenter', '.add-button', function() {
 		$(this).closest('form').find('.item-field').addClass('border-toggle-blue');
 	})
 	.on('mouseleave', '.add-button', function() {
@@ -20,14 +22,30 @@ $(document).ready(function() {
 	.on('mouseleave', '.item-field', function() {
 		$(this).closest('form').find('.add-button').removeClass('border-toggle-blue-cross-blue');
 	});
-	//toggle blue border on button when item field focused
+	//toggle blue border on button when item field focused and toggle notes field
 	$('.item-field').focusin(function () {
 		$(this).closest('form').find('.add-button').addClass('border-toggle-blue-cross-blue-2');
-		$(this).closest('.shopping-list-app').find('.notes-div').addClass('display-block');
+		$(this).closest('#shopping-list-app').find('#notes-div').show();
+		$(this).closest('#add-item-div').css('padding-bottom','5px');
 	})
-	.focusout(function () {
+	.focusout(function() {
 		$(this).closest('form').find('.add-button').removeClass('border-toggle-blue-cross-blue-2');
+		$(this).closest('#shopping-list-app').find('#notes-div').hide();
+		$(this).closest('#add-item-div').css('padding-bottom','30px');
 	});
+	$('#notes-div').focusin(function() {
+		$(this).show();
+		$(this).closest('#shopping-list-app').find('#add-item-div').css('padding-bottom','5px');
+		$(this).closest('#shopping-list-app').find('.item-field').addClass('border-toggle-blue');
+		$(this).closest('#shopping-list-app').find('.add-button').addClass('border-toggle-blue-cross-blue-2');
+	})
+	.focusout(function() {
+		$(this).hide();
+		$(this).closest('#shopping-list-app').find('#add-item-div').css('padding-bottom','30px');
+		$(this).closest('#shopping-list-app').find('.item-field').removeClass('border-toggle-blue');
+		$(this).closest('#shopping-list-app').find('.add-button').removeClass('border-toggle-blue-cross-blue-2');
+	});
+
 	//tick item on list
 	$('#list').on('click', '.untick', function(event) {
 		event.preventDefault();
@@ -49,6 +67,18 @@ $(document).ready(function() {
 	$('#list').on('click', '.delete', function(event) {
 		event.preventDefault();
 		$(this).closest('.item').remove();
+	});
+	//Open extra notes
+	$('#list').on('click', '.arrow-right', function(event) {
+		$(this).hide();
+		$(this).closest('.item').find('.arrow-down').show().css('display', 'inline-block');
+		$(this).closest('.item').find('.extra-notes').show(100);
+	});
+	//Close extra notes
+	$('#list').on('click', '.arrow-down', function(event) {	
+		$(this).hide();
+		$(this).closest('.item').find('.arrow-right').show().css('display', 'inline-block');
+		$(this).closest('.item').find('.extra-notes').hide(100);
 	});
 
 })
